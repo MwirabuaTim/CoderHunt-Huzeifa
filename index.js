11b1fd3fbc249640cf815b1464026928c6dc3e75
techytimo <techytimo@blackpearl.localdomain>
2014-08-27 16:19:17 +0300

    testing the ports

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express'),
 	path = require('path'),
-    logger = require('morgan'),
+    logger = require('morgan');
     //devs = require('./routes/thedevs');
     
 var app = express();
 
 //Set port
-app.set('port', process.env.PORT || 5205);
-     
+// app.set('port', process.env.PORT || 80);
+
+// all environments
+var port = process.env.VMC_APP_PORT || 3000;
+var host = process.env.VCAP_APP_HOST || 'localhost';
+app.set('port', process.env.PORT || port);
+
+
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
